40506b00c3070526494abc6ef3d800f7ffef1d9b
iiMihyar <talal.ala.1420@hotmail.com>
2022-12-17 04:16:53 +0300

    remove submodule

---
+++ udagram/udagram-api/src/config/config.ts
@@ -0,0 +1,22 @@
+import path from "path"
+require('dotenv').config({ path: path.resolve(__dirname, '../../src/.env') })
+// ENV variables 
+// - AWS_ACCESS_KEY_ID
+// - AWS_SECRET_ACCESS_KEY
+// Are Also needed
+
+export const config = {
+  username: `${process.env.POSTGRES_USERNAME}`,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  aws_region: process.env.AWS_REGION,
+  dbPort:Number(process.env.DB_PORT),
+  port:Number(process.env.PORT),
+  aws_profile: process.env.AWS_PROFILE,
+  aws_media_bucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
