f6baf3ba4106546fb9c7c00488d5f95cd6cc44a1
iiMihyar <talal.ala.1420@hotmail.com>
2022-12-30 00:56:54 +0300

    changes

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -9,3 +9,5 @@ export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
+
+eb setenv POSTGRES_USERNAME="postgres" POSTGRES_PASSWORD="abcd1234" POSTGRES_DB="postgres" POSTGRES_HOST="database-1.cjxky2zyosob.us-east-1.rds.amazonaws.com" AWS_REGION="us-east-1" AWS_BUCKET="tb-udagram" JWT_SECRET="mihyar" RDS_DIALECT="postgres" DB_PORT="5432" ENV="dev" AWS_ACCESS_KEY_ID="AKIASIKXQXOC3ABC6GFW" AWS_SECRET_ACCESS_KEY="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"
